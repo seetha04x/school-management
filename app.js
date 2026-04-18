7c66224cb6484e47a405adb296506c528dde2b76
seetha04x <seetha04x@gmail.com>
2026-04-18 12:01:55 +0530

    Fix

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ require("dotenv").config();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
+    host: process.env.MYSQLHOST,     // Changed from DB_HOST
+    user: process.env.MYSQLUSER,     // Changed from DB_USER
+    password: process.env.MYSQLPASSWORD, // Changed from DB_PASSWORD
+    database: process.env.MYSQLDATABASE, // Changed from DB_NAME
+    port: process.env.MYSQLPORT || 3306
 });
 connection.connect((err) => {
     if (err) {
